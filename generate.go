5cc9e01836b4d14bcf6b7bea94fd21d3e538cd41
Muhammad Rizki Fikriansyah <muhammadrizki.fikriansyah@agfa.com>
2019-08-06 11:33:24 +0200

    init commit

---
+++ generate.go
@@ -0,0 +1,21 @@
+package randstring
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+
+// Generate the random string according to n
+func Generate(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
+	}
+	return string(b)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/klovack/randstring
+
+go 1.12
